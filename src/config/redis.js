dc14207a6f54bf43a3ec1a45aa9da2d04579e978
u-kevine <k.uwisanga@alustudent.com>
2025-10-27 08:26:16 +0000

    updates

---
+++ src/config/redis.js
@@ -0,0 +1,22 @@
+const redis = require('redis');
+require('dotenv').config();
+
+const redisClient = redis.createClient({
+  host: process.env.REDIS_HOST,
+  port: process.env.REDIS_PORT,
+  password: process.env.REDIS_PASSWORD,
+});
+
+redisClient.on('connect', () => {
+  console.log('Redis connected successfully');
+});
+
+redisClient.on('error', (err) => {
+  console.error('Redis connection error:', err);
+});
+
+(async () => {
+  await redisClient.connect();
+})();
+
+module.exports = redisClient;
